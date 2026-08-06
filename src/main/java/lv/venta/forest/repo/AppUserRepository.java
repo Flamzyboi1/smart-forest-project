f8b34c9052794a018f54fee0fd05dc7e52e38f77
Flamzyboi1 <favourobidiaso@gmail.com>
2026-08-07 01:34:05 +0300

    Create AppUserRepository.java

---
+++ src/main/java/lv/venta/forest/repo/AppUserRepository.java
@@ -0,0 +1,13 @@
+package lv.venta.forest.repo;
+
+import lv.venta.forest.model.AppUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
